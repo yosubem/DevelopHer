02381d05c2646c05c235ee1453a6aeea86ce90a0
Emily <yosubem@gmail.com>
2023-04-01 22:08:46 +0300

    Exercise 3

---
+++ Sundays/DatatStructures/ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApp3;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace DataStructureExercise {
+	class Program {
+		static void Main() {
+
+			StackedQueue stackedQueue = new StackedQueue();
+
+			stackedQueue.Enqueue(314);
+			stackedQueue.Enqueue(42);
+			stackedQueue.Enqueue(2048);
+
+			stackedQueue.PrintQueue();
+
+			stackedQueue.Dequeue();
+
+			stackedQueue.PrintQueue();
+			
+		}
+
+	}
+}
---
+++ Sundays/DatatStructures/ConsoleApp3/StackedQueue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp3 {
+	internal class StackedQueue{
+		//I am not that good with naming stuff
+		Stack<int> stackPrime;
+		Stack<int> stackTemp;
+
+		public StackedQueue() {
+			stackPrime = new Stack<int>();
+			stackTemp = new Stack<int>();
+		}
+
+
+		/*Pop every element from stack A and Push to stack B. Then, Push the new element to stack A.
+		 Pop every element from stac B and Push to stack A.*/
+		public void Enqueue(int item) {
+			
+			while(stackPrime.Count > 0) {
+				stackTemp.Push(stackPrime.Pop());
+			}
+
+			stackPrime.Push(item);
+			
+			while(stackTemp.Count > 0) {
+				stackPrime.Push(stackTemp.Pop());
+			}
+		}
+
+		public int Dequeue() {
+			return stackPrime.Pop();	
+		}
+
+		public void PrintQueue() {
+			foreach(int e in stackPrime) {
+				Console.WriteLine(e);
+			}
+		}
+	}
+	
+}
