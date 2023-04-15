4023ac2466f6bd3716e294fb793ea7914d37b58e
Emily <yosubem@gmail.com>
2023-04-15 13:32:02 +0300

    First bug fixed

--- Sundays/Week12-Debug/Assets/Scripts/ColorView.cs
+++ Sundays/Week12-Debug/Assets/Scripts/ColorView.cs
@@ -6,37 +6,37 @@ using Random = UnityEngine.Random;
 
 public class ColorView : BaseView
 {
-    [SerializeField] private Image _image;
-    // Start is called before the first frame update
+	[SerializeField] private Image _image;
+	// Start is called before the first frame update
    
 
-    protected override void onClickInternal()
-    {
-        Debug.Log("clicked!");
-        float r = Random.Range(0f, 1f);
-        float g = Random.Range(0f, 1f);
-        float b = Random.Range(0f, 1f);
-        _image.color = new Color(r, g, b);
-    }
+	protected override void onClickInternal()
+	{
+		Debug.Log("clicked!");
+		float r = Random.Range(0f, 1f);
+		float g = Random.Range(0f, 1f);
+		float b = Random.Range(0f, 1f);
+		_image.color = new Color(r, g, b);
+	}
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-        System.Random random = new System.Random();
-        int randomNumber = random.Next();
-        if (randomNumber % 1000 == 0) // checking if it divides by 10000 or not 
-        {
-            try
-            {
-                throw new Exception();
-            }
-            catch (Exception e)
-            {
-                Debug.LogError($"error: {e} {randomNumber}\n");
-            }
-        }
-       
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		
+		System.Random random = new System.Random();
+		int randomNumber = random.Next();
+		if (randomNumber % 1000 == 0) // checking if it divides by 10000 or not 
+		{
+			/*try
+			{
+				throw new Exception();
+			}
+			catch (Exception e)
+			{
+				Debug.LogError($"error: {e} {randomNumber}\n");
+			}*/
+		}
+	   
+	}
 }
